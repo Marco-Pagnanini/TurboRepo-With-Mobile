da2abf6bb77b6192556d1e52696fa979619358c2
Marco Pagnanini <marco.pagnanini2004@gmail.com>
2025-11-30 14:09:23 +0100

    feat: add Expo-router Style

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from '@repo/ui/button';
-export default function App() {
-    return (<View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Button appName="mobile">Click me</Button>
-      <StatusBar style="auto"/>
-    </View>);
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <Text>Open up app/index.tsx to start working on your app!</Text>
+            <Button appName="mobile">Click me</Button>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
