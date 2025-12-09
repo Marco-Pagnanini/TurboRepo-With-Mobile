78974af414d4ffa900233fbd2a16307ab515c27a
Marco Pagnanini <marco.pagnanini2004@gmail.com>
2025-12-09 11:55:32 +0100

    feat: add CIAOOO

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         <div className={styles.page}>
             <Button appName="web" >
                 <p>Ciao</p>
+                <p>CIAOOO</p>
             </Button>
         </div>
     );
